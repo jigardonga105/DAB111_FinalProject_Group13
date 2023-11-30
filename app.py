5a0480c0ec8b3271410c3d91997f432dc450c24a
shreyas joshi <79828542+RazorX6666@users.noreply.github.com>
2023-11-30 14:27:03 -0500

    Database connection and project setup file with all routes

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from pathlib import Path
+import sqlite3
+
+base_path = Path.cwd()
+db_name = "MCU.db"
+file_path = base_path / db_name
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/data")
+def data():
+    conn = sqlite3.connect(file_path)
+    cursor = conn.cursor()
+    movies = cursor.execute("SELECT * FROM movies").fetchall()
+
+
+    columns_info = cursor.execute("PRAGMA table_info(movies)").fetchall()
+    columns = [column[1] for column in columns_info]
+    conn.close()
+
+    return render_template("data_table_fillin.html", movies = movies, columns = columns)
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ database/build_db.py
@@ -0,0 +1,39 @@
+# Required packages
+from pathlib import Path
+import sqlite3
+import pandas as pd
+
+# use pathlib to get current working directory
+path = Path.cwd()
+
+
+def create_db(db_name, filename, table_name):
+    # create a path to the data file
+    file_path = path / filename
+
+    # create a connection to the database
+    conn = sqlite3.connect(db_name)
+    # create a cursor
+    cur = conn.cursor()
+
+    # read in the data 
+    movies = pd.read_csv(file_path)
+    
+    # insert the data into the specified table
+    movies.to_sql(table_name, conn, index=False, if_exists="replace")
+    
+    # execute a select statement as f-string and print results to verify insertion
+    rows = cur.execute(f"SELECT * FROM {table_name}").fetchall()
+    print(rows)
+
+    # commit the changes to the database
+    conn.commit()
+    # close the connection
+    conn.close()
+
+
+if __name__=="__main__":
+    db_name = "MCU.db"
+    filename = "MCU_Data.csv"
+    table_name = "movies"
+    create_db(db_name, filename, table_name)
